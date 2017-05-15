58fdaef991fe48fdaca8e91bd5d7df672e8bf6a6
fuzhutech <fuzhutech@163.com>
2017-05-15 22:51:23 +0800

    Initial commit

---
+++ src/main/java/com/baidu/ueditor/hunter/FileManager.java
@@ -0,0 +1,124 @@
+package com.baidu.ueditor.hunter;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Map;
+
+import org.apache.commons.io.FileUtils;
+
+import com.baidu.ueditor.PathFormat;
+import com.baidu.ueditor.define.AppInfo;
+import com.baidu.ueditor.define.BaseState;
+import com.baidu.ueditor.define.MultiState;
+import com.baidu.ueditor.define.State;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FileManager {
+
+	private static Logger logger = LoggerFactory.getLogger(FileManager.class);
+
+	private String dir = null;
+	private String rootPath = null;
+	private String[] allowFiles = null;
+	private int count = 0;
+	
+	public FileManager ( Map<String, Object> conf ) {
+
+		this.rootPath = (String)conf.get( "rootPath" );
+		this.dir = this.rootPath + (String)conf.get( "dir" );
+		this.allowFiles = this.getAllowFiles( conf.get("allowFiles") );
+		this.count = (Integer)conf.get( "count" );
+		
+	}
+	
+	public State listFile ( int index ) {
+		logger.info("FileManager listFile");
+		
+		File dir = new File( this.dir );
+		logger.info("listFile dir:"+dir);
+		State state = null;
+
+		if ( !dir.exists() ) {
+			return new BaseState( false, AppInfo.NOT_EXIST );
+		}
+		
+		if ( !dir.isDirectory() ) {
+			return new BaseState( false, AppInfo.NOT_DIRECTORY );
+		}
+		
+		Collection<File> list = FileUtils.listFiles( dir, this.allowFiles, true );
+		
+		if ( index < 0 || index > list.size() ) {
+			state = new MultiState( true );
+		} else {
+			Object[] fileList = Arrays.copyOfRange( list.toArray(), index, index + this.count );
+			state = this.getState( fileList );
+		}
+		
+		state.putInfo( "start", index );
+		state.putInfo( "total", list.size() );
+		
+		return state;
+		
+	}
+	
+	private State getState ( Object[] files ) {
+		
+		MultiState state = new MultiState( true );
+		BaseState fileState = null;
+		
+		File file = null;
+		
+		for ( Object obj : files ) {
+			if ( obj == null ) {
+				break;
+			}
+			file = (File)obj;
+			fileState = new BaseState( true );
+			fileState.putInfo( "url", PathFormat.format( this.getPath( file ) ) );
+
+			logger.info("this.getPath:"+this.getPath(file));
+			state.addState( fileState );
+		}
+		
+		return state;
+		
+	}
+	
+	private String getPath ( File file ) {
+		
+		String path = file.getAbsolutePath();
+
+		path = path.replace('/','\\');
+		logger.info("file.getAbsolutePath:"+path);
+		logger.info("this.rootPath:"+this.rootPath);
+		
+		return path.replace( this.rootPath.replace('/','\\'), "/" );
+		
+	}
+	
+	private String[] getAllowFiles ( Object fileExt ) {
+		
+		String[] exts = null;
+		String ext = null;
+		
+		if ( fileExt == null ) {
+			return new String[ 0 ];
+		}
+		
+		exts = (String[])fileExt;
+		
+		for ( int i = 0, len = exts.length; i < len; i++ ) {
+			
+			ext = exts[ i ];
+			exts[ i ] = ext.replace( ".", "" );
+			
+		}
+		
+		return exts;
+		
+	}
+	
+}
